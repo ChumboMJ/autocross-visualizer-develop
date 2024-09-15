38781e968cfdff94f3321dccb85a164e4e37099c
Tim Forste <tforste@petsafe.net>
2024-09-14 20:05:08 -0700

    first commit

---
+++ AutoXDisplay.DAL/AutoXWriter.cs
@@ -0,0 +1,19 @@
+﻿using AutoXDisplay.DAL.Entities;
+using CsvHelper;
+using System.Globalization;
+
+namespace AutoXDisplay.DAL
+{
+    public class AutoXWriter : IAutoXWriter
+    {
+        public Task WriteCsvFileAsyc(IEnumerable<RaceResultEntity> raceResultEntities, string filePath = "_artifacts/autoXtest2.csv")
+        {
+            //TODO: Check if the folder exists, create if possible
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                return csv.WriteRecordsAsync(raceResultEntities);
+            }
+        }
+    }
+}
---
+++ AutoXDisplay.DAL/Interfaces/IAutoXReader.cs
@@ -0,0 +1,11 @@
+﻿using AutoXDisplay.DAL.Entities;
+
+namespace AutoXDisplay.DAL;
+
+public interface IAutoXReader
+{
+    Task<IEnumerable<RaceResultEntity>> SyncEventResultsFromWebAsync();
+    
+    //TODO: Implement a function that will use .CSV Uploads
+    Task SyncEventResultsFromCsvAsync();
+}
